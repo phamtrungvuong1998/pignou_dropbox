1d0c9778a4361070039c1c3bb6dec568369fa409
mor-vuongpt <baclv@mor.com.vn>
2024-02-01 10:52:43 +0700

    update code

--- vue/src/auth/page/LoginPage.vue
+++ vue/src/auth/page/LoginPage.vue
@@ -11,7 +11,7 @@ export default {
     data() {
         return {
             dataLogin: {
-                _token: this.$store.state.token,
+                _name: this.$store.state.token,
                 email: null,
                 password: null,
             },
